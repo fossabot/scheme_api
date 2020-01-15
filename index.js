a10cd127a145f9c12f89d7f18ffc216dd8cd71cc
Yomi Adenaike <adenaikeyomi@gmail.com>
2020-01-15 01:43:25 +0000

    removed unused services

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 require("dotenv").config();
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const compression = require("compression");
 const cors = require("cors");
 const app = express();
 const morgan = require("morgan");
+
 const shiftRoutes = require("./routes/shiftRoutes");
 const userRoutes = require("./routes/userRoutes");
 const requestRoutes = require("./routes/requestRoutes");
 const clientRoutes = require("./routes/clientRoutes");
 const messengerRoutes = require("./routes/messengerRoutes");
 const notificationRoutes = require("./routes/notificationRoutes");
-const serviceRoutes = require("./routes/serviceRoutes");
+
 const templateRoutes = require("./routes/templateRoutes");
 const verifyToken = require("./middlewares/verifyToken");
 
@@ -24,6 +26,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(compression());
 app.use(morgan("combined"));
+app.use(express.static("./upload"));
 
 // Routing
 app.use("/users", userRoutes);
@@ -32,16 +35,16 @@ app.use("/requests", verifyToken, requestRoutes);
 app.use("/clients", clientRoutes);
 app.use("/messenger", verifyToken, messengerRoutes);
 app.use("/notifications", verifyToken, notificationRoutes);
-app.use("/auth", serviceRoutes);
 app.use("/templates", verifyToken, templateRoutes);
 
 // test
-app.get("/test", (req, res) => {
+app.get("test", (req, res) => {
   res.json({
     message: "HELLO"
   });
 });
 
+// Server init
 app.listen(7070, () => {
   console.log(
     `Running | Environment : ${process.env.NODE_ENV || "Development"}`
